60818b873357c2264ae9e59cc625e1896870ebd9
lsl <a15838261357@sina.com>
2018-10-15 09:40:54 +0800

    按钮样式调整

--- js/common.js
+++ js/common.js
@@ -325,7 +325,6 @@ function autofill() {
 	var bianhao = getQueryString('bh');
 	var gongsi = getQueryString("gs");
 
-
 	//alert(shijian);
 	//alert(document.getElementById("Txt_1"));
 	//alert(document.getElementById("Txt_3"));
@@ -333,107 +332,135 @@ function autofill() {
 	document.getElementById("gs").value = gongsi;
 
 }
+
 function autofill1() {
 	var xiangmu = getQueryString('xm');
 	var gongwei = getQueryString("gw");
 	var yuangong = getQueryString("yg");
 
-	
 	//alert(shijian);
 	//alert(document.getElementById("Txt_1"));
 	//alert(document.getElementById("Txt_3"));
 	document.getElementById("xm").value = xiangmu;
 	document.getElementById("gw").value = gongwei;
-	document.getElementById("yg").value =yuangong ;
+	document.getElementById("yg").value = yuangong;
 
 }
-		var a = null;
+var a = null;
 
-			function showPicture(imgFile) {
-				/*获取上传文件的路径，并赋给img标签*/
-				a = window.URL.createObjectURL(imgFile.files[0]);
+function showPicture(imgFile) {
+	/*获取上传文件的路径，并赋给img标签*/
+	a = window.URL.createObjectURL(imgFile.files[0]);
 
-				return a;
-			}
-
-			function look() {
-				document.getElementById("newImage").src = a;
-
-			}
-			 function dis_confirm(){
- 	var r = confirm("尚未绑定手机号，点击确认绑定!")
- 	if(r==true){
- 		window.location.href="self_phone_bind.html"
- 	}
- }
- function keep(){
- 	 	var r = confirm("是否保存？")
- 	if(r==true){
- 		alert("保存成功！");
- 		
- 	}
- }
-function sub(){
-	 	var r = confirm("是否上报？")
- 	if(r==true){
- 		alert("上报成功！");
- 		window.location.href="all_application.html";
- 	}
+	return a;
 }
-function revise(){
+
+function look() {
+	document.getElementById("newImage").src = a;
+
+}
+
+function dis_confirm() {
+	var r = confirm("尚未绑定手机号，点击确认绑定!")
+	if(r == true) {
+		window.location.href = "self_phone_bind.html"
+	}
+}
+
+function keep() {
+	var r = confirm("是否保存？")
+	if(r == true) {
+		alert("保存成功！");
+
+	}
+}
+
+function sub() {
+	var r = confirm("是否上报？")
+	if(r == true) {
+		alert("上报成功！");
+		window.location.href = "all_application.html";
+	}
+}
+
+function revise() {
 	alert("该任务已被锁定，无法修改!")
 }
-function attendenceSub(){
-	 	var r = confirm("是否上报？")
- 	if(r==true){
- 		alert("上报成功！");
- 		window.location.href="list_attendence.html";
- 	}
+
+function attendenceSub() {
+	var r = confirm("是否上报？")
+	if(r == true) {
+		alert("上报成功！");
+		window.location.href = "list_attendence.html";
+	}
 }
-function complainSub(){
-	 	var r = confirm("是否上报？")
- 	if(r==true){
- 		alert("上报成功！");
- 		window.location.href="list_complain.html";
- 	}
+
+function complainSub() {
+	var r = confirm("是否上报？")
+	if(r == true) {
+		alert("上报成功！");
+		window.location.href = "list_complain.html";
+	}
 }
-function workSub(){
-	 	var r = confirm("是否上报？")
- 	if(r==true){
- 		alert("上报成功！");
- 		window.location.href="list_work.html";
- 	}
+
+function workSub() {
+	var r = confirm("是否上报？")
+	if(r == true) {
+		alert("上报成功！");
+		window.location.href = "list_work.html";
+	}
 }
-function superviseSub(){
-	 	var r = confirm("是否上报？")
- 	if(r==true){
- 		alert("上报成功！");
- 		window.location.href="list_supervise.html";
- 	}
+
+function superviseSub() {
+	var r = confirm("是否上报？")
+	if(r == true) {
+		alert("上报成功！");
+		window.location.href = "list_supervise.html";
+	}
 }
-      function today(){//构建方法
-        var today=new Date();//new 出当前时间
-        var h=today.getFullYear();//获取年
-        var m=today.getMonth()+1;//获取月
-        var d=today.getDate();//获取日
-        var H = today.getHours();//获取时
-        var M = today.getMinutes();//获取分
-        var S = today.getSeconds();//获取秒
-       //返回 年-月-日 时:分:秒
-        document.getElementById("today").value=h+"-"+m+"-"+d+" "+H+":"+M+":"+S; //将获取到的 年-月-日 时:分:秒 赋给input文本输入框
+
+function today() { //构建方法
+	var today = new Date(); //new 出当前时间
+	var h = today.getFullYear(); //获取年
+	var m = today.getMonth() + 1; //获取月
+	var d = today.getDate(); //获取日
+	var H = today.getHours(); //获取时
+	var M = today.getMinutes(); //获取分
+	var S = today.getSeconds(); //获取秒
+	//返回 年-月-日 时:分:秒
+	document.getElementById("today").value = h + "-" + m + "-" + d + " " + H + ":" + M + ":" + S; //将获取到的 年-月-日 时:分:秒 赋给input文本输入框
 
 }
-			function fillSelect() {
-				var obj = document.getElementById("s1");
-				var s2 = document.getElementById("s2");
-				for(var i = 2010; i < 2054; i++) {
 
-					var op = new Option(i, i);
-					obj.add(op);
-				}
-				for(var y = 1; y < 13; y++) {
-					var a = new Option(y, y);
-					s2.add(a);
-				}
-			}
+function fillSelect() {
+	var obj = document.getElementById("s1");
+	var s2 = document.getElementById("s2");
+	for(var i = 2010; i < 2054; i++) {
 
+		var op = new Option(i, i);
+		obj.add(op);
+	}
+	for(var y = 1; y < 13; y++) {
+		var a = new Option(y, y);
+		s2.add(a);
+	}
+}
+
+function HideOrShowFont(obj) {
+	if(obj == "a1") {
+		document.getElementById("a1").style.color = "red";
+		
+		document.getElementById("a2").style.color = "dodgerblue";
+		document.getElementById("a3").style.color = "dodgerblue";
+	} else if(obj == 'a2') {
+		document.getElementById("a1").style.color = "dodgerblue";
+		document.getElementById("a2").style.color = "red";
+			
+
+		document.getElementById("a3").style.color = "dodgerblue";
+	} else if(obj == 'a3') {
+		document.getElementById("a1").style.color = "dodgerblue";
+		document.getElementById("a2").style.color = "dodgerblue";
+		document.getElementById("a3").style.color = "red";
+	}
+}
